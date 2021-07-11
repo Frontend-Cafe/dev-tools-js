f8e4e2b6fa202010e3d9c9ae239f109432228063
Santiago Santana <santiago.santana@naturalth.it>
2021-07-11 18:04:09 -0300

    pruebas

--- src/main.js
+++ src/main.js
@@ -1,3 +1,39 @@
-import './style.css'
+import vaccinesData from '../assets/covid.json'
 
-document.querySelector('#app').innerHTML = `<h1>Hello Vite!</h1>`
+/**
+ * @param {{
+ *   jurisdiccion_nombre: string
+ *   vacuna_nombre: string
+ *   primera_dosis_cantidad: string
+ *   segunda_dosis_cantidad: string
+ * }} vaccineData
+ */
+function vaccineToHtml(vaccineData) {
+  const {
+    vacuna_nombre,
+    jurisdiccion_nombre,
+    primera_dosis_cantidad,
+    segunda_dosis_cantidad,
+  } = vaccineData
+
+  const div = document.createElement('div')
+  const pNombre = document.createElement('p')
+  pNombre.innerText = vacuna_nombre
+  pNombre.classList.add('vacuna-nombre')
+
+  const pJurisdiccion = document.createElement('p')
+  pJurisdiccion.innerText = jurisdiccion_nombre
+  pJurisdiccion.classList.add('vacuna-jurisdiccion')
+
+  div.appendChild(pNombre)
+  div.appendChild(pJurisdiccion)
+
+  return div
+}
+
+const html = vaccinesData.map(vaccineToHtml)
+const app = document.querySelector('#app')
+
+html.forEach((h) => {
+  app.appendChild(h)
+})
